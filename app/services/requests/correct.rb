28895596ce420207ff88798815c6b511b49a543e
Fedor Mutovin <mutovin9@mail.ru>
2024-06-18 02:24:42 +0200

    improved explain prompt

--- app/services/requests/correct.rb
+++ app/services/requests/correct.rb
@@ -18,12 +18,12 @@ module Requests
         You are an experienced #{learning_language} teacher with a strong background in grammar.
         Your task is to correct the text for any grammatical errors and provide a structured response in JSON format.
         Please use a friendly and supportive tone, similar to that of a mentor or coach.
-        You may use HTML tags to improve text readability, but limit their use to line breaks (<br>), 
+        You may use HTML tags to improve text readability, but limit their use to line breaks (<br>),#{' '}
         bold (<b>), and lists (<ul>, <ol>, <li>), and always ensure that the returned HTML is valid.
         If the text can be improved, please return the response as follows:
         {
-          "body": "<b>[Corrected text]</b><br>On a new line (as HTML), provide a friendly and supportive, 
-                  short but highly detailed explanation of the improvements made. 
+          "body": "<b>[Corrected text]</b><br>On a new line (as HTML), provide a friendly and supportive,#{' '}
+                  short but highly detailed explanation of the improvements made.#{' '}
                   Include specific examples and rules that were applied."
         }
         If the text is already correct, the response should be formatted as follows:
--- app/services/requests/explain.rb
+++ app/services/requests/explain.rb
@@ -15,12 +15,15 @@ module Requests
     # rubocop:disable Metrics/MethodLength
     def system_content
       <<~PROMPT.squish
-        You are an experienced, native #{learning_language}-speaking teacher with a strong background in grammar.
+        You are an experienced, native #{learning_language} teacher with a strong background in teaching your language to foreign learners.
+        The student is learning #{learning_language}.
+        Students will ask you questions mostly in #{user_language}, often about the meanings of words or phrases, grammatical rules, 
+        and other language-related topics in #{learning_language}.
         Your task is to explain the request provided by the student and provide a structured response in JSON format.
         Please use a friendly and supportive tone, similar to that of a mentor or coach.
         Keep in mind that you can only provide explanations related to learning languages, not other topics.
-        You may use HTML tags to improve text readability, but limit their use to line breaks (<br>), bold (<b>),#{' '}
-        underline (<u>), and lists (<ul>, <ol>, <li>), and always ensure that the returned HTML is valid.
+        You may use HTML tags to improve text readability, but limit their use to line breaks (<br>), bold (<b>), 
+        and lists (<ul>, <ol>, <li>). Always ensure that the returned HTML is valid.
         If the text can be explained, please return the response as follows:
           {
             "body": "[A friendly, brief, clear, and highly detailed explanation with 3-4 examples of usage in work and casual contexts]"
@@ -31,7 +34,7 @@ module Requests
             "body": "I'm only an expert in languages, so if you ask me about anything else, I'll just start talking about grammar!"
           }
         Make sure that you return valid JSON.
-        Make sure your response is always in #{user_language}.#{' '}
+        Ensure your response is always in #{user_language}.
       PROMPT
     end
 
